33dd60ce51fed9d46a53b22610261f3e7f631fde
unknown <hertzrafael@hotmail.com>
2025-09-06 13:53:01 -0300

    fixed files to work with months and years

--- transform.py
+++ transform.py
@@ -4,13 +4,17 @@ gold = pd.read_csv('files/XAU_USD.csv')
 gold['Data'] = pd.to_datetime(gold['Data'], dayfirst=True)
 
 (gold
-    .assign(Ano=lambda x: x['Data'].dt.year)
+    .assign(
+        Ano=lambda x: x['Data'].dt.year,
+        Mes=lambda x: x['Data'].dt.month
+    )
     .astype({'Último': str})
+    .query(f'Ano >= 2007 & Ano <= 2022')
     .assign(Último=lambda x: x['Último'].str.replace('.', '').str.replace(',', '.'))
-    .filter(items=['Data', 'Último', 'Ano'])
+    .filter(items=['Data', 'Último', 'Ano', 'Mes'])
     .astype({'Último': 'float64'})
-    .groupby('Ano')
+    .groupby(['Ano', 'Mes'])
     .mean()
     .reset_index()
-    .to_csv('files/gold_price.csv')
+    .to_csv('files/gold_price_monthly.csv', index=False)
 )
